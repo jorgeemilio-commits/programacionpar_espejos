355ca870a0369e2235fd6376e027cd74779f7bfc
Jorge Emilio <jorge.emilio.chavez.lizarraga@gmail.com>
2025-10-02 13:22:55 -0700

    first commit

---
+++ example/programacionpar_espejos_example.dart
@@ -0,0 +1,6 @@
+import 'package:programacionpar_espejos/programacionpar_espejos.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/programacionpar_espejos_test.dart
@@ -0,0 +1,16 @@
+import 'package:programacionpar_espejos/programacionpar_espejos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
