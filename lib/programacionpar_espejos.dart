a2db374d9d0535f51b97df17660126a8b4273719
emilio_com <jorgeemilio7777@gmail.com>
2025-10-02 14:28:00 -0700

    Mas pruebas y arreglo de errores

--- lib/programacionpar_espejos.dart
+++ lib/programacionpar_espejos.dart
@@ -3,18 +3,37 @@ export 'src/programacionpar_espejos_base.dart';
 
 String ChecarEspejo(List<int> arreglo) {
   int n = arreglo.length;
+  int longMaxima = 0;
 
-  for (int i = 0; i < n ~/ 2; i++) {
-
-    if (arreglo[i] != arreglo[n - i - 1]) {
-
-      return 'No es un espejo';
-      
+  for (int i = 0; i < n; i++) {
+    if (arreglo[i] == arreglo[n - 1 - i]) {
+      longMaxima++;
+    } else {
+      break;
     }
   }
-  return 'Es un espejo';
 
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      List<int> sublista = arreglo.sublist(i, j + 1);
 
+      bool esEspejo = true;
+      for (int k = 0; k < sublista.length ~/ 2; k++) {
+        if (sublista[k] != sublista[sublista.length - 1 - k]) {
+          esEspejo = false;
+          break;
+        }
+      }
 
+      if (esEspejo && sublista.length > longMaxima) {
+        longMaxima = sublista.length;
+      }
+    }
+  }
 
+  if (longMaxima > 0) {
+    return 'La longitud del espejo  más grande es: $longMaxima';
+  } else {
+    return 'No se encontraron espejos de longitud mayor a 0';
+  }
 }
--- test/programacionpar_espejos_test.dart
+++ test/programacionpar_espejos_test.dart
@@ -4,18 +4,46 @@ import 'package:test/test.dart';
 void main() {
   group('Prueba de espejos', () {
 
-    test('Hay 2 espejos de diferentes tamaños', () {
-      final numeros = [1, 2, 3, 4, 3, 2, 1];
+    test('Cantidad de numeros repetidos', () {
+      final numeros = [1, 2, 3, 4, 6 , 8 , 4, 3, 2, 1];
 
-      expect(ChecarEspejo(numeros), "Es un espejo");
+      expect(ChecarEspejo(numeros), "La longitud del espejo  más grande es: 4");
     });
 
-     test('no hay espejos de diferentes tamaños', () {
-      final numeros = [1, 6, 3, 4, 3, 2, 7];
+     test('no hay espejos', () {
+      final numeros = [1, 2, 3, 4, 5];
 
-      expect(ChecarEspejo(numeros), "No es un espejo");
+      expect(ChecarEspejo(numeros), "No se encontraron espejos de longitud mayor a 0");
     });
 
+     test('espejo completo', () {
+      final numeros = [7, 1, 2, 3, 2, 1, 7];
 
+      expect(ChecarEspejo(numeros), "La longitud del espejo  más grande es: 7");
+    });
+
+     test('espejo en los extremos', () {
+      final numeros = [5, 9, 1, 2, 3, 1, 9, 5];
+
+      expect(ChecarEspejo(numeros), "La longitud del espejo  más grande es: 3");
+    });
+
+     test('espejo en el centro', () {
+      final numeros = [4, 6, 8, 1, 2, 1, 8, 6, 4];
+
+      expect(ChecarEspejo(numeros), "La longitud del espejo  más grande es: 9");
+    });
+
+     test('espejo con un solo elemento', () {
+      final numeros = [3];
+
+      expect(ChecarEspejo(numeros), "La longitud del espejo  más grande es: 1");
+    });
+
+     test('arreglo vacío', () {
+      final numeros = <int>[];
+
+      expect(ChecarEspejo(numeros), "No se encontraron espejos de longitud mayor a 0");
+    });
   });
 }
